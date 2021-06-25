87d6a72ca5895229e400f9c05fda9e75f35efe53
Henrik-Adam <henrik.adam@flagbit.com>
2021-06-25 14:33:18 +0200

    add nav for index and change folder structure

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../css/main_page.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8"> 
+    <title>Index</title>
+</head>
+<body>
+
+<div class="nav">
+    <a href="#home" class="active">Home</a>
+    <a href="#search">Produkte</a>
+    <a href="#search">Account</a>
+</div>
+
+<div style="padding-left:16px">
+  <h2>Responsive Top Navigation with Centered and Right-Aligned Links</h2>
+  <p>Resize the browser window to see the responsive effect.</p>
+</div>
+
+</body>
+</html>
+
